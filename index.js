26602ef427322c6a67d9e708397bf8fc9c8f77c5
Mideolabanji <olabanjikaosara@yahoo.com>
2021-12-01 20:44:43 +0100

    added custom JS to add underline to each section when clicked

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-function underlineAboutLink() {
-  homepageLink.ClassList.remove("underline");
-  aboutLink.classList.add("underline");
+function underlineAbout() {
+  homepageLink.classList.remove("underline");
+  underlineWorkLink.classList.remove("underline");
+  underlineContactLink.classList.remove("underline");
+  underlineAboutLink.classList.add("underline");
 }
 
-let aboutLink = document.querySelector("#aboutLink");
-aboutLink.addEventListener("click", underlineAboutLink);
+function underlineWork() {
+  homepageLink.classList.remove("underline");
+  underlineAboutLink.classList.remove("underline");
+  underlineContactLink.classList.remove("underline");
+  underlineWorkLink.classList.add("underline");
+}
+
+function underlineContact() {
+  homepageLink.classList.remove("underline");
+  underlineAboutLink.classList.remove("underline");
+  underlineWorkLink.classList.remove("underline");
+  underlineContactLink.classList.add("underline");
+}
+
+let homepageLink = document.querySelector("#homepageLink");
+let underlineAboutLink = document.querySelector("#aboutLink");
+let underlineWorkLink = document.querySelector("#workLink");
+let underlineContactLink = document.querySelector("#contactLink");
+
+underlineAboutLink.addEventListener("click", underlineAbout);
+underlineWorkLink.addEventListener("click", underlineWork);
+underlineContactLink.addEventListener("click", underlineContact);
